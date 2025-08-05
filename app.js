7cec784d6756fc373c63d47b70abc8ecaa3f077b
LeoMosqueda <leo.mosq@hotmail.com>
2025-08-05 14:46:33 -0300

    Agregar amigos con validacion

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];//variable del amigo secreto
+let amigos = [];//lista de nombres
 
-function agregarAmigo() {
-  const nombre = document.getElementById("amigo").value;
+function agregarAmigo() {//funcion al boton añadir
+  const nombre = document.getElementById("amigo").value;//guardado de nombre escrito en el campo
 
   if (nombre === "") {
     alert("Por favor, inserte un nombre.");
-    return;
+    return;//esto detiene la funcion
   }
 
-  amigos.push(nombre);
+  amigos.push(nombre);//nombre de la caja de nombres
 
   //Mostrar la lista en pantalla 
   const lista = document.getElementById("listaAmigos");
   lista.innerHTML = ""; 
 
-  for (let i = 0; i < amigos.length; i++) {
+  for (let i = 0; i < amigos.length; i++) {//limpiado de caja para evitar duplicados y vovler a poner todos los nombres
     lista.innerHTML += "<li>" + amigos[i] + "</li>";
   }
 
-  //limpiar el input
+  //limpiar el input/campo
   document.getElementById("amigo").value = "";
 }
 
